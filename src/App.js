1cef40efbb536f26668809a6a68b4508f16b6bc8
miwzerking <0865697@students.dadeschools.net>
2021-01-10 17:30:14 -0500

    finished logic and client, waiting for animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import Message from './component/Message';
+import db from './config/firebase';
+import firebase from 'firebase';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState('');
+	const [messages, setMessages] = useState([]);
+	const [userName, setUserName] = useState('');
+
+	useEffect(() => {
+		db.collection('messages')
+			.orderBy('timestamp', 'asc')
+			.onSnapshot(snapshot => {
+				setMessages(snapshot.docs.map(doc => doc.data()));
+			});
+	}, []);
+
+	useEffect(() => {
+		setUserName(prompt('Plese enter your name'));
+	}, []);
+
+	const sendMessage = e => {
+		e.preventDefault();
+		db.collection('messages').add({
+			text: input,
+			username: userName,
+			timestamp: firebase.firestore.FieldValue.serverTimestamp()
+		});
+		setInput('');
+	};
+
+	return (
+		<div className='App'>
+			<h2>Welcome {userName}</h2>
+			<form>
+				<FormControl>
+					<InputLabel>Enter a message....</InputLabel>
+					<Input value={input} onChange={e => setInput(e.target.value)} />
+					<Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>
+						Send Message
+					</Button>
+				</FormControl>
+			</form>
+
+			{messages.map(obj => (
+				<Message message={obj} username={userName} />
+			))}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+	apiKey: 'AIzaSyAsx-ANM8_Ia3kr8vJ6Nj8JAETXBqNMe6g',
+	authDomain: 'messenger-clone-ff016.firebaseapp.com',
+	projectId: 'messenger-clone-ff016',
+	storageBucket: 'messenger-clone-ff016.appspot.com',
+	messagingSenderId: '126840255110',
+	appId: '1:126840255110:web:0f7c94b5787839567f9707',
+	measurementId: 'G-8YRT7N8TGJ'
+});
+
+const db = firebaseApp.firestore();
+
+export default db;
